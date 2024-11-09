21cbc9badcb5bc3b6b4db2f71c16be24bce26c57
wf34 <wf34@users.noreply.github.com>
2024-11-09 16:13:18 +0000

    test py adopted to test buldozer scene

--- test.py
+++ test.py
@@ -55,13 +55,13 @@ def f(rays, models, embeddings, dataset):
 
 
 def test(args):
-    img_wh = (800, 800)
+    img_wh = (200, 200)
     
     # dataset = dataset_dict['llff'] \
     #           ('/home/ubuntu/data/nerf_example_data/my/silica4/', 'test_train', spheric_poses=True,
     #            img_wh=img_wh)
     
-    dataset = dataset_dict['blender']('/home/wf34/projects/nerf_pl/lego/', 'test', img_wh=img_wh)
+    dataset = dataset_dict['blender']('/home/wf34/projects/nerf_pl/lego2/', 'test', img_wh=img_wh)
 
     embedding_xyz = Embedding(args.N_emb_xyz)
     embedding_dir = Embedding(args.N_emb_dir)
@@ -71,7 +71,7 @@ def test(args):
     nerf_fine = NeRF(in_channels_xyz=6*args.N_emb_xyz+3,
                      in_channels_dir=6*args.N_emb_dir+3)
     
-    ckpt_path = 'ckpts/testing_train_run/epoch=0.ckpt'
+    ckpt_path = 'ckpts/testing_train_run2/epoch=15.ckpt'
     
     load_ckpt(nerf_coarse, ckpt_path, model_name='nerf_coarse')
     load_ckpt(nerf_fine, ckpt_path, model_name='nerf_fine')
@@ -109,7 +109,7 @@ def test(args):
     plt.subplot(223)
     plt.title('depth')
     plt.imshow(visualize_depth(depth_pred).permute(1,2,0))
-    plt.savefig('logs/testing_train_run/test.png')
+    plt.savefig('logs/testing_train_run2/test.png')
 
 
 if '__main__' == __name__:
