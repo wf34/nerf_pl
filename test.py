74865a16f3feb3c21d9884087ccfe4a99470b2e6
wf34 <wf34@users.noreply.github.com>
2024-11-09 13:20:45 +0000

    fixed a single image test run

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import torch
+from tap import Tap
+
 from utils import *
 from collections import defaultdict
 import matplotlib.pyplot as plt
@@ -20,9 +22,13 @@ N_importance = 64
 use_disp = False
 chunk = 1024*32*4
 
+class TestArgs(Tap):
+    N_emb_xyz:int = 10
+    N_emb_dir:int = 4
+
 
 @torch.no_grad()
-def f(rays):
+def f(rays, models, embeddings, dataset):
     """Do batched inference on rays using chunk."""
     B = rays.shape[0]
     results = defaultdict(list)
@@ -41,14 +47,14 @@ def f(rays):
                         test_time=True)
 
         for k, v in rendered_ray_chunks.items():
-            results[k] += [v]
+            results[k] += [v.cpu()]
 
     for k, v in results.items():
         results[k] = torch.cat(v, 0)
     return results
 
 
-def test():
+def test(args):
     img_wh = (800, 800)
     
     # dataset = dataset_dict['llff'] \
@@ -57,13 +63,15 @@ def test():
     
     dataset = dataset_dict['blender']('/home/wf34/projects/nerf_pl/lego/', 'test', img_wh=img_wh)
 
-    embedding_xyz = Embedding(3, 10)
-    embedding_dir = Embedding(3, 4)
+    embedding_xyz = Embedding(args.N_emb_xyz)
+    embedding_dir = Embedding(args.N_emb_dir)
     
-    nerf_coarse = NeRF()
-    nerf_fine = NeRF()
+    nerf_coarse = NeRF(in_channels_xyz=6*args.N_emb_xyz+3,
+                       in_channels_dir=6*args.N_emb_dir+3)
+    nerf_fine = NeRF(in_channels_xyz=6*args.N_emb_xyz+3,
+                     in_channels_dir=6*args.N_emb_dir+3)
     
-    ckpt_path = 'ckpts/exp2/epoch=05.ckpt'
+    ckpt_path = 'ckpts/testing_train_run/epoch=0.ckpt'
     
     load_ckpt(nerf_coarse, ckpt_path, model_name='nerf_coarse')
     load_ckpt(nerf_fine, ckpt_path, model_name='nerf_fine')
@@ -71,14 +79,14 @@ def test():
     nerf_coarse.cuda().eval()
     nerf_fine.cuda().eval()
     
-    models = [nerf_coarse, nerf_fine]
-    embeddings = [embedding_xyz, embedding_dir]
+    models = {'coarse': nerf_coarse, 'fine': nerf_fine}
+    embeddings = {'xyz': embedding_xyz, 'dir': embedding_dir}
 
     sample = dataset[0]
     rays = sample['rays'].cuda()
     
     t = time.time()
-    results = f(rays)
+    results = f(rays, models, embeddings, dataset)
     torch.cuda.synchronize()
     
     print('one pass time:', time.time()-t)
@@ -101,7 +109,9 @@ def test():
     plt.subplot(223)
     plt.title('depth')
     plt.imshow(visualize_depth(depth_pred).permute(1,2,0))
-    plt.savefig('test.png')
+    plt.savefig('logs/testing_train_run/test.png')
+
 
 if '__main__' == __name__:
-    test()
+    args = TestArgs().parse_args()
+    test(args)
